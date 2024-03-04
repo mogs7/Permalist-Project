7d47283b3be163a4563c704d43ea291cc54781f6
Miguel Molina <statixr2@gmail.com>
2024-03-04 16:42:43 +0800

    Add functionality working

--- index.js
+++ index.js
@@ -1,28 +1,48 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
 
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "permalist",
+  password: "miguel123",
+  port: 5432,
+})
+
+db.connect();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let items = [
-  { id: 1, title: "Buy milk" },
-  { id: 2, title: "Finish homework" },
-];
+let items = [];
 
-app.get("/", (req, res) => {
-  res.render("index.ejs", {
-    listTitle: "Today",
-    listItems: items,
-  });
+app.get("/", async (req, res) => {
+  try {
+    const result = await db.query("SELECT * FROM items ORDER BY id ASC");
+    items = result.rows;
+    
+    res.render("index.ejs", {
+      listTitle: "Today",
+      listItems: items,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-app.post("/add", (req, res) => {
+app.post("/add", async (req, res) => {
   const item = req.body.newItem;
-  items.push({ title: item });
-  res.redirect("/");
+  // items.push({ title: item });
+  try {
+    await db.query("INSERT INTO items (title) VALUES ($1)",[item]);
+    res.redirect("/");
+  } catch (error) {
+   console.log(error);
+  }
 });
 
 app.post("/edit", (req, res) => {});
